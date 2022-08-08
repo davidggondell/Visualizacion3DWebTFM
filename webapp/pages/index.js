e095ceea755bc2282d6f149a02a8a258b92da569
davidggondell <david.ggondell@udc.es>
2022-08-08 19:23:11 +0200

    Rendering 1500 earths

--- webapp/pages/index.js
+++ webapp/pages/index.js
@@ -1,16 +1,32 @@
+import React, { useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { PinkBox } from "../components/3DComponents/PinkBox";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Stats } from "@react-three/drei";
 import { Box } from "@mui/material";
-import { Sun } from "../components/3DComponents/Sun";
+import { EarthAnimated } from "../components/3DComponents/EarthAnimated";
 
 export default function Home() {
+  const createEarths = () => {
+    const copyArray = new Array(1500).fill();
+    return (
+      <>
+        {copyArray.map((j, i) => {
+          const x = Math.floor(Math.random() * 300) - 150;
+          const y = Math.floor(Math.random() * 300) - 150;
+          const z = Math.floor(Math.random() * 300) - 150;
+          return <EarthAnimated key={i} position={[x, y, z]} />;
+        })}
+      </>
+    );
+  };
+
   return (
     <Box sx={{ width: "100vw", height: "100vh" }}>
       <Canvas>
         <OrbitControls />
         <ambientLight intensity={0.5} />
-        <Sun />
+        <EarthAnimated />
+        {createEarths()}
+        <Stats />
       </Canvas>
     </Box>
   );
